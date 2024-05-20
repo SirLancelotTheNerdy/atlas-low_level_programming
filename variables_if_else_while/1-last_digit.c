9721f3f08ab8036ec05b96aac70888e9f0221672
root <lancelot.gay@atlasschool.com>
2024-05-20 02:21:31 +0000

    task 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,12 +14,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	v = (n % 10);
-	printf("Last digit of %d is %d", n, v);
+	printf("Last digit of %d is %d ", n, v);
 	if (v > 5)
 	{
 		printf("and is greater than 5\n");
 	}
-	 if (v < 6 && v != 0);
+	 if (v < 6 && v != 0)
         {
                 printf("and is less than 6 and not 0\n");
         }
