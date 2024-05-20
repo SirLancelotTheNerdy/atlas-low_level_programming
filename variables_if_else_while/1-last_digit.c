ccc82a48ef85a2769f9237a191667f14088ac935
root <lancelot.gay@atlasschool.com>
2024-05-20 02:07:28 +0000

    fj

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int n;
-
+		int v:
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	v = (n % 10);
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("and is zero\n");
 	}
-	else (v < 6 && != 0)
+	else (v < 6 && v != 0)
 	{
 		printf("and is less than 6 and not 0\n");
 	}
