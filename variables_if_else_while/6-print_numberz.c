9ebdc891a008a00ce5687f646bdb38b337ae405c
root <lancelot.gay@atlasschool.com>
2024-05-20 05:09:53 +0000

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - header prints the numbers.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int l;
+	int o;
+
+	for (l = 0 ; l <= 9 ; l++)
+
+	{
+		o = (l + 48);
+
+		putchar(o);
+	}
+		putchar('\n');
+return (0);
+}
