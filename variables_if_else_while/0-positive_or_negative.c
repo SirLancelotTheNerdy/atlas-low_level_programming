a549b86f010747961b451a618feb46ed1ab3a1d3
root <lancelot.gay@atlasschool.com>
2024-05-20 01:10:18 +0000

    betty fixes

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/**
+ * main - header print random number abd defines it.
+ * Return: 0
+ */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
@@ -10,10 +12,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	printf("%d is positive\n")
+	printf("%d is positive\n", n)
 	else if (n < 0)
-	printf("%d is negative\n")
+	printf("%d is negative\n", n)
 	else
-	printf("%d is zero\n")
+	printf("%d is zero\n", n)
 	return (0);
 }
