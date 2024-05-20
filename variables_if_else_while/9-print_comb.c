3900f65e51120f84179008509c752a1d50784412
root <lancelot.gay@atlasschool.com>
2024-05-20 06:31:15 +0000

    task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - header prints the numbers.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int l;
+	int o;
+
+	for (l = 0 ; l <= 9 ; l++)
+
+	{
+		o = (l + 48);
+
+		putchar(o);
+	
+	if (l != 9)
+	{
+		putchar(',');
+
+		putchar(' ');
+	}
+	}
+		putchar('\n');
+return (0);
+}
