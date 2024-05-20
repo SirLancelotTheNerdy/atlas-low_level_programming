63465c0f20de3b3b23aa559e7882addc2c32a6af
root <lancelot.gay@atlasschool.com>
2024-05-20 04:27:37 +0000

    task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - header prints the leters.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int l;
+	for (l = 0 ; l <= 9 ; l++)
+
+	{
+		printf("%d\n", l);
+	}
+return (0);
+}
